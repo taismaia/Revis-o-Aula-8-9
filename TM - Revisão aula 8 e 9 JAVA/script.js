3cbe241c89d81367943b47e15056a706e8204f2c
taismaia <148501573+taismaia@users.noreply.github.com>
2024-05-23 17:40:08 -0300

    Add files via upload

---
+++ TM - Revisão aula 8 e 9 JAVA/script.js
@@ -0,0 +1,42 @@
+
+// const email = document.querySelector("#email")
+// const senha = document.querySelector("#senha")
+
+// const botao = document.querySelector("#botao")
+
+// const erro = document.querySelector("#erro")
+// const sucesso = document.querySelector("#sucesso")
+
+
+// botao.addEventListener("click", ()=>{
+//     const lista_de_usuarios = [
+//         ["Joao", "joao@email.com", "123456"],
+//         ["Maria", "maria25@email.com", "deusefiel"],
+//         ["Ana", "anaJulia@email.com", "456789"],
+//         ["pedro", "ped@email.com", "pedrimDaTuf"],
+//         ["AbeL", "abel.jr@email.com", "10081995"],
+//         ["Amanda", "amandKaroline@email.com", "1ate8"],
+//         ]
+    
+//     let = deuCerto = false
+
+//     for(usuario_da_vez of lista_de_usuarios){
+//         if(usuario_da_vez[1] === email.value && usuario_da_vez[2] === senha.value){
+//             sucesso.textContent = "Login efetuado com sucesso"
+//             // sucesso.className = "deuBom"
+//             sucesso.classList.add("deuBom")
+//             deuCerto = true
+//             erro.textContent = ""
+//             erro.classList.remove("deuRuim")
+//             // erro.className = ""
+//             email.value = ""
+//             senha.value = ""
+//         }
+//     }
+
+//     if(!deuCerto){
+//         erro.textContent = "E-mail/Senha inválidos"
+//         erro.className = "deuRuim"
+//         sucesso.textContent = ""
+//     }
+// })
